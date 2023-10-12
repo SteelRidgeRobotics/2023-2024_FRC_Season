6cc87a0eb29c664d19cc9ee2f42a36c6d712acd4
TheSavageDemon <thesavagedemon7@gmail.com>
2023-10-12 16:19:24 -0700

    Added constants and code introduced in training robot file

--- TrunkOrTreatRobot/robot.py
+++ TrunkOrTreatRobot/robot.py
@@ -1,6 +1,23 @@
 import wpilib
 import magicbot
 import ctre
+import constants
+
 
 class TrunkOrTreatRobot(magicbot.MagicRobot):
-    
+
+    def createObjects(self):
+
+        self.armMotor = ctre.TalonFX(constants.MOTORID) #Arm motor
+        self.grabberSolenoid = wpilib.Solenoid(constants.SOLENOIDID1) #Not sure what ID is for solenoids. Our guess is that it's wpilib.solenoid
+        self.wristSolenoid = wpilib.Solenoid(constants.SOLENOIDID2) #Same issue here
+
+        self.driverController = wpilib.XboxController(constants.DRIVERPORT) #We might have to change the controller if we're using the guitar hero
+
+    def teleopPeriodic(self):
+
+        pass #Code needed here
+
+if __name__ == "__main__":
+
+    wpilib.run(TrunkOrTreatRobot)
