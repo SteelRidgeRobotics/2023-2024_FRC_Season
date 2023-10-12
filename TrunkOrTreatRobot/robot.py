23463bacfb804bee38ed0550567078b52a86d492
Kings_Code6942 <mjmurten@gmail.com>
2023-10-12 16:52:39 -0700

    Created all objects

--- TrunkOrTreatRobot/robot.py
+++ TrunkOrTreatRobot/robot.py
@@ -9,10 +9,8 @@ class TrunkOrTreatRobot(magicbot.MagicRobot):
     def createObjects(self):
 
         self.armMotor = ctre.TalonFX(constants.MOTORID) #Arm motor
-        self.grabberSolenoid = wpilib.DoubleSolenoid(constants.SOLENOIDID1) #Not sure what ID is for solenoids. Our guess is that it's wpilib.solenoid
-        self.wristSolenoid = wpilib.DoubleSolenoid(constants.SOLENOIDID2) #Same issue here
-
-        self.driverController = wpilib.XboxController(constants.DRIVERPORT) #We might have to change the controller if we're using the guitar hero
+        self.grabberSolenoid = wpilib.DoubleSolenoid(constants.CLAWSOLENOIDID, wpilib.PneumaticsModuleType.CTREPCM, constants.SOLENOIDFORWARDCHANNEL, constants.SOLENOIDREVERSECHANNEL) #Not sure what ID is for solenoids. Our guess is that it's wpilib.solenoid
+        self.functionsController = wpilib.XboxController(constants.FUNCTIONSPORT) #We might have to change the controller if we're using the guitar hero
 
     def teleopPeriodic(self):
 
