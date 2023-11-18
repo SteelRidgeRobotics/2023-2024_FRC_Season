5ebb4db8d498b01a0401524c35bea5eddb85072b
PickleFace5 <caden.dalley@icloud.com>
2023-11-17 17:29:29 -0800

    Added speed, translation, and rotation multipliers

--- Larry/subsystems/swerve_drive.py
+++ Larry/subsystems/swerve_drive.py
@@ -62,7 +62,13 @@ class SwerveDrive(commands2.SubsystemBase):
         
         self.PDP = wpilib.PowerDistribution(0, wpilib.PowerDistribution.ModuleType.kCTRE)
 
-    def turnWheel(self, module: SwerveWheel, direction: float, magnitude: float) -> None:
+        # Movement modifiers
+        self.speedMultiplier = self.defaultSpeedMultiplier = 1.0
+        self.rotationMultiplier = self.defaultRotationMultiplier = 1.0
+        self.translationMultiplier = self.defaultTranslationMultiplier = 1.0
+
+    def turnWheel(self, module: SwerveWheel, direction: float, magnitude: float, 
+                  applySpeedMultiplier: bool=False) -> None:
         """
         Turns a swerve wheel based on the provided direction and 
         magnitude.
@@ -76,6 +82,8 @@ class SwerveDrive(commands2.SubsystemBase):
 
         # Magnitude clamp between -1 and 1
         magnitude = max(-1.0, min(1.0, magnitude))
+        if applySpeedMultiplier:
+            magnitude *= self.speedMultiplier
 
         module.turnToOptimizedAngle(direction)
 
@@ -84,7 +92,8 @@ class SwerveDrive(commands2.SubsystemBase):
         
         module.move(magnitude)
 
-    def translate(self, direction: float, magnitude: float):
+    def translate(self, direction: float, magnitude: float, 
+                  applyTranslationMultiplier: bool=True, applySpeedMultiplier: bool=True):
         """
         Allows for moving up, down, left, and right without rotating 
         the robot.
@@ -94,13 +103,18 @@ class SwerveDrive(commands2.SubsystemBase):
         :param magnitude: The magnitude the robot should travel at 
         (ranging from 0 to 1, 1 being 100% power)
         """
+        if applyTranslationMultiplier:
+            magnitude *= self.translationMultiplier
+        if applySpeedMultiplier:
+            magnitude *= self.speedMultiplier
 
         self.turnWheel(self.leftFrontSwerveModule, direction, magnitude)
         self.turnWheel(self.leftRearSwerveModule, direction, magnitude)
         self.turnWheel(self.rightFrontSwerveModule, direction, magnitude)
         self.turnWheel(self.rightRearSwerveModule, direction, magnitude)
 
-    def translateAndTurn(self, translationX: float, translationY: float, rotX: float) -> None:
+    def translateAndTurn(self, translationX: float, translationY: float, rotX: float, 
+                         applyTranslationMultiplier: bool=True, applyRotationMultiplier: bool=True, applySpeedModifier: bool=True) -> None:
         """
         This is the default movement method for swerve drive.
 
@@ -116,6 +130,13 @@ class SwerveDrive(commands2.SubsystemBase):
         """
         translationX *= -1
         rotX *= -1
+
+        # Add non-speed multipliers
+        if applyTranslationMultiplier:
+            translationX *= self.translationMultiplier
+            translationY *= self.translationMultiplier
+        if applyRotationMultiplier:
+            rotX *= self.rotationMultiplier
 
         # Field Orientaated Drive (aka complicated math so the robot doesn't rotate while we translate or somthin idrk)
         temp = translationY * math.cos(self.getYaw() * (math.pi / 180)) + translationX * math.sin(self.getYaw() * (math.pi / 180))
@@ -157,6 +178,13 @@ class SwerveDrive(commands2.SubsystemBase):
         if translationX == 0 and translationY == 0 and rotX == 0:
             self.stopAllMotors()
             return
+        
+        # Speed modifiers
+        if applySpeedModifier:
+            topRight[0] *= self.speedMultiplier
+            topLeft[0] *= self.speedMultiplier
+            bottomLeft[0] *= self.speedMultiplier
+            bottomRight[0] *= self.speedMultiplier
 
         # Turn wheels :D
         self.turnWheel(self.leftFrontSwerveModule, topLeft[1], topLeft[0])
@@ -164,7 +192,7 @@ class SwerveDrive(commands2.SubsystemBase):
         self.turnWheel(self.leftRearSwerveModule, bottomLeft[1], bottomLeft[0])
         self.turnWheel(self.rightRearSwerveModule, bottomRight[1], bottomRight[0])
 
-    def turnInPlace(self, turnPower: float):
+    def turnInPlace(self, turnPower: float, applyRotationMultiplier: bool=True, applySpeedMultiplier: bool=True) -> None:
         """
         Sets all motors to pre-defined angles to allow for optimized 
         turning back and forth.
@@ -172,6 +200,11 @@ class SwerveDrive(commands2.SubsystemBase):
         :param turnPower: The magnitude of the turning (ranging from 
         -1 to 1, 1 being 100% power forward)
         """
+        if applyRotationMultiplier:
+            turnPower *= self.rotationMultiplier
+        if applySpeedMultiplier:
+            turnPower *= self.speedMultiplier
+
         self.turnWheel(self.leftFrontSwerveModule, 45.0, turnPower)
         self.turnWheel(self.rightFrontSwerveModule, 135.0, turnPower)
         self.turnWheel(self.rightRearSwerveModule, 225.0, turnPower)
@@ -235,3 +268,81 @@ class SwerveDrive(commands2.SubsystemBase):
         self.rightFrontSpeed.setSelectedSensorPosition(0.0, constants.kPIDLoopIdx, constants.ktimeoutMs)
         self.rightRearDirection.setSelectedSensorPosition(0.0, constants.kPIDLoopIdx, constants.ktimeoutMs)
         self.rightRearSpeed.setSelectedSensorPosition(0.0, constants.kPIDLoopIdx, constants.ktimeoutMs)
+
+    def setSpeedMultiplier(self, newSpeed: float) -> None:
+        """
+        Sets the new speed multiplier.
+
+        The speed multiplier is factored into all turnWheel functions.
+
+        Values between 0-1 are accepted, 0 being 0% power and 1 being maximum power.
+        """
+        self.speedMultiplier = max(-1.0, min(1.0, newSpeed))
+
+    def getSpeedMultiplier(self) -> float:
+        """
+        Gets the speed multiplier.
+
+        The speed multiplier is factored into all turnWheel functions.
+        """
+        return self.speedMultiplier
+    
+    def getDefaultSpeedMultiplier(self) -> float:
+        """
+        Gets the default speed multiplier.
+
+        The speed multiplier is factored into all turnWheel functions.
+        """
+        return self.defaultSpeedMultiplier
+    
+    def setRotationMultiplier(self, newSpeed: float) -> None:
+        """
+        Sets the new rotation multiplier.
+
+        The rotation multiplier is factored into all rotation-related calculations.
+
+        Values between 0-1 are accepted, 0 being 0% power and 1 being maximum power.
+        """
+        self.rotationMultiplier = max(-1.0, min(1.0, newSpeed))
+
+    def getRotationMultiplier(self) -> float:
+        """
+        Gets the rotation multiplier.
+
+        The rotation multiplier is factored into all rotation-related calculations.
+        """
+        return self.rotationMultiplier
+    
+    def getDefaultRotationMultiplier(self) -> float:
+        """
+        Gets the default rotation multiplier.
+
+        The rotation multiplier is factored into all rotation-related calculations.
+        """
+        return self.defaultRotationMultiplier
+    
+    def setTranslationMultiplier(self, newSpeed: float) -> None:
+        """
+        Sets the new translation multiplier.
+
+        The translation multiplier is factored into all translation-related calculations.
+
+        Values between 0-1 are accepted, 0 being 0% power and 1 being maximum power.
+        """
+        self.translationMultiplier = max(-1.0, min(1.0, newSpeed))
+
+    def getTranslationMultiplier(self) -> float:
+        """
+        Gets the translation multiplier.
+
+        The translation multiplier is factored into all translation-related calculations.
+        """
+        return self.translationMultiplier
+    
+    def getDefaultTranslationMultiplier(self) -> float:
+        """
+        Gets the default translation multiplier.
+
+        The translation multiplier is factored into all translation-related calculations.
+        """
+        return self.defaultTranslationMultiplier
